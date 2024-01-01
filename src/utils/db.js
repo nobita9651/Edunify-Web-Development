d3419a926cd5bf53d96bd4b9c550c17131b1addc
Nobita9651 <abhidubey603@gmail.com>
2024-01-01 15:33:50 +0530

    env changes

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,14 +1,13 @@
 let db;
 
 if (typeof window === "undefined") {
-  // This block will only be executed on the server side
   const mysql = require("mysql2");
 
   db = mysql.createConnection({
-    host: process.env.DB_HOST || "localhost",
-    user: process.env.DB_USER || "abhi",
-    password: process.env.DB_PASSWORD || "Ijkl098765@",
-    database: process.env.DB_DATABASE || "school_db",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
   });
 
   db.connect((err) => {
