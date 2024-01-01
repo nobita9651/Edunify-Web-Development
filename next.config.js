eec5fcf45b65ed02ed23ecc323c9a59b9749ddc5
Nobita9651 <abhidubey603@gmail.com>
2024-01-01 13:25:13 +0530

    changes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
-}
-
-module.exports = nextConfig
+  exportPathMap: function () {
+    return {
+      "/": { page: "/" },
+    };
+  },
+};
