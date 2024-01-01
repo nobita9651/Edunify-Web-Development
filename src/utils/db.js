34836687464d5d84886e0945cb338ebf02cbd00b
Nobita9651 <abhidubey603@gmail.com>
2024-01-01 18:36:05 +0530

    changes

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql2");
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.NEXT_PUBLIC_DB_HOST,
+  user: process.env.NEXT_PUBLIC_DB_USER,
+  password: process.env.NEXT_PUBLIC_DB_PASSWORD,
+  database: process.env.NEXT_PUBLIC_DB_NAME,
   connectionLimit: 10,
 });
 
