9f890a1322bbea6d855d5d61c716aa906a980f84
Nobita9651 <abhidubey603@gmail.com>
2024-01-01 16:17:03 +0530

    env changes

--- src/utils/db.js
+++ src/utils/db.js
@@ -3,24 +3,19 @@ let db;
 if (typeof window === "undefined") {
   const mysql = require("mysql2");
 
-  const pool = mysql.createPool({
-    connectionLimit: 10, // Adjust as needed
+  db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
   });
 
-  db = pool.promise();
-
-  // Test the connection
-  db.getConnection((err, connection) => {
+  db.connect((err) => {
     if (err) {
       console.error("Error connecting to MySQL:", err);
       throw err;
     }
     console.log("Connected to MySQL database");
-    connection.release();
   });
 }
 
